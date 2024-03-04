39339afab3c327a9bd53b4701bac87c112c43b68
Alexander7474 <alexandre.lanternier@outlook.fr>
2024-03-04 23:33:32 +0100

    ajout d'un matrice de projection dans le shader pour preparer l'arrivé au matrice de rotation, de position et de vue

--- src/BBOP/Graphics/sceneClass.cpp
+++ src/BBOP/Graphics/sceneClass.cpp
@@ -1,5 +1,9 @@
 #include "../../../include/BBOP/Graphics/sceneClass.h"
 
+#include <glm/glm.hpp>
+#include <glm/gtc/type_ptr.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+
 Scene::Scene()
   : sceneShader(defaultVertex, defaultFragment),
     ambiantLightValue(1.0f),
@@ -30,6 +34,8 @@ void Scene::Use()
 
 void Scene::Draw(BbopDrawable& spr)
 {
+  glm::mat4 projection = glm::ortho(0.0f, 956.0f, 1044.0f, 0.0f, -1.0f, 1.0f);
+  glUniformMatrix4fv(sceneShader.getUniformLoc("projection"), 1, GL_FALSE, glm::value_ptr(projection));
   spr.Draw(renderModeLoc);
 }
 
--- src/BBOP/Graphics/shaders.cpp
+++ src/BBOP/Graphics/shaders.cpp
@@ -6,12 +6,14 @@ layout (location = 0) in vec2 aPos;
 layout (location = 1) in vec4 aColor;
 layout (location = 2) in vec2 aTexCoord;
 
+uniform mat4 projection;
+
 out vec4 outColor;
 out vec2 TexCoord;
 
 void main()
 {
-    gl_Position = vec4(aPos, 0.0, 1.0);
+    gl_Position = projection * vec4(aPos, 0.0, 1.0);
     outColor = aColor;
     TexCoord = aTexCoord;
 } 
