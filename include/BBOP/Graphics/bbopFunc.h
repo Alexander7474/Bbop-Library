6153e1561e233423b8199cbc3f5692fe89c4e402
Alexander7474 <alexandre.lanternier@outlook.fr>
2024-03-19 21:01:11 +0100

    work in progress

--- include/BBOP/Graphics/bbopFunc.h
+++ include/BBOP/Graphics/bbopFunc.h
@@ -8,9 +8,47 @@
 #include <GLFW/glfw3.h>
 #include <iostream>
 
+/**
+* @brief Fonction d'initialisation de Bbop Library
+*
+* @param[in] windowX Taille x de la fenêtre
+* @param[in] windowY Taille y de la fenêtre
+* @param[in] windowName Nom de la fenêtre
+* @param[in,out] window Fenêtre glfw à utiliser
+*
+* @see Dans bbopFunc.cpp 
+*
+* @note window dois-être un pointeur
+*/
 int bbopInit(int windowX, int windowY, const char* windowName, GLFWwindow*& window);
+
+/**
+* @brief Fonction de nettoyage de la fenêtre
+*
+* @param[in] window Fenêtre à nettoyer
+* @param[in] rgb Couleur de fond de la fenêtre
+* @param[in] alpha Transaprence du nettoyage
+*
+* @see Dans bbopFunc.cpp 
+*
+* @note window dois-être un pointeur
+*/
 void bbopCleanWindow(GLFWwindow*& window,Vector3i rgb, float alpha);
+
+/**
+* @brief Fonction de verification d'erreur d'opengl
+*
+* @see Dans bbopFunc.cpp 
+*/
 void bbopErrorCheck();
+
+/**
+* @brief Fonction d'initialisation de Bbop Library
+*
+* @param[in] wWinSize Nouvelle taille de la fenêtre 
+*
+* @see Dans bbopFunc.cpp 
+*/
 void bbopChangeWindowSize(Vector2i nWinSize);
 
 #endif // !BBOP_FUNCTION_H
